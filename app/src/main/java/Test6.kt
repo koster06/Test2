12a0f3a2309ffdf5949f8c896fd9f4d36ceeeabc
koster06 <46241269+koster06@users.noreply.github.com>
2023-02-11 19:03:10 +0300

    Добавил задачу №4a

--- app/src/main/java/Test6.kt
+++ app/src/main/java/Test6.kt
@@ -5,20 +5,44 @@
 Функция any получает предикат в качестве аргумента и возвращает true, если хотя бы один элемент
 удовлетворяет предикату.*/
 
-fun main() {
+fun main(){
 
-    val sum = {x:Int, y:Int -> x + y }
-    doOperation(3, 4, sum)                      // 7
-    doOperation(3, 4, {a:Int, b: Int -> a * b}) // 12
+    val  firstList = listOf(1,-4,-2)
+    val mult1:(Int,) -> Boolean = { it > 0 }
+    val mult2:(Int) -> Boolean = { it <0 }
+
+    val mult1List = containsIn(firstList, mult1)
+    val mult2List = containsIn(firstList, mult2)
+
+    println(mult1List)
+    println(mult2List)
 
 }
-fun doOperation(x: Int, y: Int, op: (Int, Int) ->Int){
 
-    val result = op(x, y)
-    println(result)
-}
+    fun containsIn(myList:List<Int>, any:(Int) -> Boolean ): Boolean {
+        var oK = true
+        for ( i in myList) {
+            oK = any(i)
+        }
+        return oK
+    }
 
-//fun containsIn(collection: Collection): Boolean = collection.any { TODO() }
-//fun any():Boolean {
-//TODO()
+//    val a = 0
+//    val collection = listOf<Int>(1,2,3,4,5,6,7,8,9)
+//    fun containsIn(collection: List<Int>): Boolean = collection.any { x:Int,y:Int -> Boolean }
+//    fun any(collection: List<Int>,a:Int): Boolean {
+//        for (i in collection) return i < a
+//    }
+
+//fun main() {
+//
+//    val sum = {x:Int, y:Int -> x + y }
+//    doOperation(3, 4, sum)
+//    doOperation(3, 4, {a:Int, b: Int -> a * b})
+//
+//}
+//fun doOperation(x: Int, y: Int, op: (Int, Int) ->Int){
+//
+//    val result = op(x, y)
+//    println(result)
 //}
